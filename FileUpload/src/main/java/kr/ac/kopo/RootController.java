72901d09889e83ea4681320307d4da8555d50061
Student <Student@Student-PC>
2023-12-19 10:51:06 +0900

    fileupload

---
+++ FileUpload/src/main/java/kr/ac/kopo/RootController.java
@@ -0,0 +1,54 @@
+package kr.ac.kopo;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class RootController {
+	final String uploadPath = "d:/upload/";
+
+	List<Image> list = new ArrayList<Image>();
+	
+	@GetMapping("/")
+	String index(Model model) {
+		model.addAttribute("list", list);
+		return "index";
+	}
+	
+	@ResponseBody
+	@PostMapping("/upload/ajax")
+	String upload_ajax(Image image) {
+		upload(image);
+		
+		return "OK";
+	}
+	
+	@PostMapping("/upload")
+	String upload(Image image) {
+		MultipartFile file = image.getUploadFile();
+		
+		if(file != null && !file.isEmpty()) {
+			String filename = file.getOriginalFilename();
+				
+			try {
+				file.transferTo(new File(uploadPath + filename));
+				
+				image.setFilename(filename);
+				
+				list.add(image);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}	
+		}
+		
+		return "redirect:.";
+	}
+}
